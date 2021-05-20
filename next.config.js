a58f011f50cb615e6d87d7e05425212e17048b6c
seong7 <seongjinkim1123@gmail.com>
2021-05-21 01:22:17 +0900

    chore: Add linting dependencies

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    webpack: (config) => {
-        // eslint-disable-next-line
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '~': path.resolve('./'),
-        }
+  webpack: (config) => {
+    // eslint-disable-next-line
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '~': path.resolve('./'),
+    };
 
-        config.module.rules.push({
-            test: /\.svg$/,
-            issuer: {
-                test: /\.(js)x?$/,
-            },
-            use: ['@svgr/webpack', 'url-loader'],
-        })
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js)x?$/,
+      },
+      use: ['@svgr/webpack', 'url-loader'],
+    });
 
-        return config
-    },
-}
+    return config;
+  },
+};
